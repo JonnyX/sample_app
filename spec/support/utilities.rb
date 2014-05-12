704f502d00c4c13e8d8d5a4fe96b6b88facc35a2
John Schmidt <xero99@gmail.com>
2014-05-12 15:44:40 -0500

    Complete chapter 9 exercises 1,2,3,5,6

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -21,10 +21,10 @@ def should_have_errors
 end
 
 def valid_signup
-  fill_in "Name",         with: "Example User"
-  fill_in "Email",        with: "user@example.com"
-  fill_in "Password",     with: "foobar"
-  fill_in "Confirmation", with: "foobar"
+  fill_in "Name",             with: "Example User"
+  fill_in "Email",            with: "user@example.com"
+  fill_in "Password",         with: "foobar"
+  fill_in "Confirm Password", with: "foobar"
 end
 
 def sign_in(user, options={})
